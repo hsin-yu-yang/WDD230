70bc420563b8ab18f0feec63d2204372c6a2e013
hsin-yu-yang <yan20012@byui.edu>
2023-06-05 23:10:17 -0600

    edit

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,50 +1,31 @@
-// select HTML elements in the document
-const temperature = document.querySelector('#temperature');
-const icon = document.querySelector('#windchill-icon');
-const description = document.querySelector('#windchill-description');
-const windSpeed = document.querySelector('#wind-speed');
-const windChill = document.querySelector('#wind-chill');
-
-const url = "https://api.openweathermap.org/data/2.5/weather?q=rexburg&appid=7040ea904442a45d6950ba584410ce59";
-
-async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            //console.log(data); // this is for testing the call
-            displayResults(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-function calculateWindChill(speed, temp) {
-    if (windSpeed > 3.0 & temperature <= 50) {
-        return (35.74 + .621 * temp - (35.75 * speed ** .16) + .4275 * temp * speed ** .16).toFixed(0);
-    }
-    else {
-        return "N/A"
-    }
-}
-function toUpperCamelCase(inputString) {
-    return inputString.split(" ").map(word => {
-        let firstLetter = word.charAt(0).toUpperCase();
-        return `${firstLetter}${word.slice(1)}`;
-    }).join(" ");
-}
-function displayResults(weatherdata) {
-    const description = weatherdata.weather[0].description.toLowerCase();
-    const capitalizedDescription = toUpperCamelCase(description);
-
-    temperature.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`;
-    icon.setAttribute("src", `https://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@4x.png`);
-    icon.setAttribute("alt", `${weatherdata.weather[0].description}`);
-    description.textContent = capitalizedDescription;
-    windSpeed.innerHTML = `${weatherdata.wind.speed}`;
-    windChill.innerHTML = `${calculateWindChill(weatherdata.wind.speed, weatherdata.main.temp)}`;
-}
-
-apiFetch();
+async function getweather() {
+    const apiURL =
+        'https://api.openweathermap.org/data/2.5/weather?q=taoyuan&appid=7040ea904442a45d6950ba584410ce59' +
+        'mperial&APPID=07407eccd051a7a7b4fc81e187f47771';
+    const response  = await fetch(apiURL);
+    const data      = await response.json();
+    const wSpeed = data.wind.speed;
+    const wSpeedSpan = document.querySelector('#windspeed');
+    wSpeedSpan.innerHTML = "Wind Speed: " + wSpeed.toString() + 'mph';
+    const temp = data.main.temp;
+    const windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+    const windChillSpan = document.querySelector('#windchill');
+    windChillSpan.innerHTML = "Wind Chill: " + windChill.toString() + '°F';
+    console.log(windChill);
+    console.log(data);
+    const currentTemp   = document.querySelector('#current-temp');
+    const weathericon   = document.querySelector('#weathericon');
+    const caption       = document.querySelector('figcaption');
+    currentTemp.textContent = data.main.temp.toFixed(0);
+    let imgsrc  = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let imgalt  = data.weather[0].description;
+    imgalt      = imgalt.split(' ').map(capitalize).join(' ');
+    currentTemp.textContent = data.main.temp.toFixed(0);
+    weathericon.setAttribute('src', imgsrc);
+    weathericon.setAttribute('alt', imgalt);
+    caption.innerHTML = `Currently: ${imgalt}`;
+  }
+  function capitalize(word) {
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+  }
+  window.addEventListener('load', getweather);
